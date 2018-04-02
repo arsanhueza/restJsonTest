5843931c0e2daa06f11c84f1287f606e1f33a7d3
Arturo Sanhueza <arturosanhueza@macbook-pro-de-arturo.local>
2018-04-02 15:00:03 -0300

    Initial Commit

---
+++ restApiJson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  restApiJson
+//
+//  Created by Arturo Sanhueza on 02-04-18.
+//  Copyright © 2018 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
