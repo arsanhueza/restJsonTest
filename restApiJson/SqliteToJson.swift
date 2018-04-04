5c79bcbba10f724c1a0e3202f2360489e131fd79
Arturo Sanhueza <arturosanhueza@macbook-pro-de-arturo.local>
2018-04-04 09:51:13 -0300

    agregadas clases para sync entre sqlite y son

---
+++ restApiJson/SqliteToJson.swift
@@ -0,0 +1,13 @@
+//
+//  SqliteToJson.swift
+//  restApiJson
+//
+//  Created by Arturo Sanhueza on 04-04-18.
+//  Copyright © 2018 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class SqliteToJson: NSObject {
+
+}
