a07c6eaf8ab317ea78a6996acfa910f78fddaa9c
Arturo Sanhueza <arturosanhueza@macbook-pro-de-arturo.local>
2018-04-09 13:25:55 -0300

    limpieza del código

--- restApiJson/ViewController.swift
+++ restApiJson/ViewController.swift
@@ -22,44 +22,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        //calling the function that will fetch the json
         getJsonFromUrl();
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-    //this function is fetching the json from URL
     func getJsonFromUrl(){
-        //creating a NSURL
         let url = NSURL(string: URL_HEROES)
         
-        //fetching the data from the url
         URLSession.shared.dataTask(with: (url as URL?)!, completionHandler: {(data, response, error) -> Void in
             
             print(data)
 
             if let jsonObj = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary {
                 
-                //printing the json in console
                 print(jsonObj!.value(forKey: "peliculas")!)
                 
-                //getting the avengers tag array from json and converting it to NSArray
                 if let heroeArray = jsonObj!.value(forKey: "peliculas") as? NSArray {
-                    //looping through all the elements
                     for heroe in heroeArray{
                         
-                        //converting the element to a dictionary
                         if let heroeDict = heroe as? NSDictionary {
                             
-                            //getting the name from the dictionary
                             if let name = heroeDict.value(forKey: "nombre") {
                                 
-                                //adding the name to the array
                                 self.nameArray.append((name as? String)!)
                             }
                             
@@ -68,8 +55,6 @@ class ViewController: UIViewController {
                 }
                 
                 OperationQueue.main.addOperation({
-                    //calling another function after fetching the json
-                    //it will show the names to label
                     self.showNames()
                 })
             }
@@ -77,10 +62,8 @@ class ViewController: UIViewController {
     }
     
     func showNames(){
-        //looing through all the elements of the array
         for name in nameArray{
             
-            //appending the names to label
             labelTest.text = labelTest.text! + name + "\n";
         }
     }
